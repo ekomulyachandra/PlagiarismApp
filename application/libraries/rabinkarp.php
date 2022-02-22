9841e3dd47869ca103470475a3b53aa560df1732
ekomulyachandra <ekomulyachandra99@gmail.com>
2022-02-23 02:13:35 +0700

    fix rabin karp 16

--- application/libraries/rabinkarp.php
+++ application/libraries/rabinkarp.php
@@ -57,6 +57,7 @@ class Rabinkarp
                 $hashGram[] = $this->rollingHash($teks);
             }
         }
+        error_reporting(E_ALL ^ (E_NOTICE | E_WARNING));
         return $hashGram;
     }
 
@@ -73,6 +74,7 @@ class Rabinkarp
                 }
             }
         }
+        error_reporting(E_ALL ^ (E_NOTICE | E_WARNING));
         return $value;
     }
 
@@ -85,11 +87,8 @@ class Rabinkarp
             $ascii = floatval(ord($string[$i]));
             $x     = floatval(pow($basis, $pjgKarakter - ($i + 1)));
             $hash += $ascii * $x;
-            var_dump($ascii);
-            var_dump($x);
-            var_dump($hash);
         }
-
+        error_reporting(E_ALL ^ (E_NOTICE | E_WARNING));
         return $hash;
     }
 
@@ -118,6 +117,7 @@ class Rabinkarp
                 $hashUdahDicek[] = $hash1[$i];
             }
         }
+        error_reporting(E_ALL ^ (E_NOTICE | E_WARNING));
         return $fingerprint;
     }
 
@@ -129,6 +129,7 @@ class Rabinkarp
         foreach ($clear as $row) {
             $hasilunique[] = $row;
         }
+        error_reporting(E_ALL ^ (E_NOTICE | E_WARNING));
         return $hasilunique;
     }
 
@@ -139,6 +140,7 @@ class Rabinkarp
         // echo count($hash1);
         // echo "<br>";
         // echo count($hash2);
+
         $similarity = number_format(((2 * count($hasilunique) / (count($hash1) + count($hash2))) * 100), 2, '.', '');
 
         if ($similarity > 100) {
@@ -146,6 +148,7 @@ class Rabinkarp
         } else {
             $output = $similarity;
         }
+        error_reporting(E_ALL ^ (E_NOTICE | E_WARNING));
         return $output;
     }
 }
