195e72de5034b80b7e9058bf91a81edb395c5b3e
ekomulyachandra <ekomulyachandra99@gmail.com>
2022-02-22 16:23:01 +0700

    change baseurl

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/PlagiarismApp';
+$config['base_url'] = 'http://3.94.173.93/PlagiarismApp';
 
 /*
 |--------------------------------------------------------------------------
