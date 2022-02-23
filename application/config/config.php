afe08513dcbdc920a9e8e9d188031ebd4ce66283
ekomulyachandra <ekomulyachandra99@gmail.com>
2022-02-23 09:46:17 +0700

    fix rabin karp 20

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://54.173.110.83';
+$config['base_url'] = 'http://3.86.31.81';
 
 /*
 |--------------------------------------------------------------------------
